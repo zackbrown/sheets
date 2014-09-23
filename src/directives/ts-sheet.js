0a097d211a0155d90ce9b0bcf25b865084f26419
Zack Brown <z@charybrown.com>
2014-09-23 12:24:46 -0700

    feat: commit initial functioning directive

---
+++ src/directives/ts-sheet.js
@@ -0,0 +1,31 @@
+/**
+ * @ngdoc directive
+ * @name tsSheet
+ * @module ts.sheets
+ * @restrict A
+ * @param {string} tsSheet the name of the sheet you want to apply to this DOM subtree
+ * ts-sheet allows you to apply a Sheet to a DOM node's 'subtree
+ * Use it in conjunction with $media.$sheet to apply responsive, declarative, and reactive animations or DOM-manipulation to your application.
+ */
+angular.module('ts.sheets')
+  .directive('tsSheet', ["$media", function ($media) {
+    return {
+      restrict: 'A',
+      scope: false,
+      compile: function () {
+        return {
+          post: function (scope, element, attrs) {
+            //TODO:  $observe tsSheet to support dynamic sheet binding.  Will need to clean up (probably using $media.$clearSheets)
+            //TODO:  support arrays of multiple sheets per element (scope.$eval(attrs.tsSheet))
+            scope.$$postDigest(function(){
+              $media.$applySheet(element, attrs.tsSheet);
+            });
+
+            scope.$on('$destroy', function(){
+              $media.$clearSheets(element);
+            })
+          }
+        };
+      }
+    };
+  }]);
